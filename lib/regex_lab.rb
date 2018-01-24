693b4baf3e8a041a2f5ea521d2955aeae9e14bcf
Kyle Harris <k_harris95@live.com>
2018-01-24 07:19:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.match(/[aeiou]+\w/) ? true : false
+  word.match(/[aeiou]+\w/) ? true : return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  word.
 end
 
 def words_five_letters_long(text)
